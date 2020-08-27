5d5cac3733f409d94274a62a5fee7bb241069d81
Bartek <bartlomiejnawoj@gmail.com>
2020-08-27 12:13:32 +0200

    s3:37 Working with props and props.children

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React, { Component } from 'react'
-import './App.css'
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
-	render() {
-		return (
-			<div className="App">
-				<h1>Hi, I'm a react App</h1>
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a react App</h1>
+        <p>This is realy working</p>
+        <Person name="Max" age="18">
+          My hobby is playing piano
+        </Person>
+        <Person name="Manu" age="12" />
+        <Person name="Mimo" age="21" />
+      </div>
+    );
+  }
 }
 
-export default App
+<Person />;
+export default App;
