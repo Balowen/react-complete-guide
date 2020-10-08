a0511cca522e0884b66d44cc20e7fbf57d695154
Bartek <bartlomiejnawoj@gmail.com>
2020-10-08 22:59:24 +0200

    s7:114 contextType and useContext() for functional components

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -12,9 +12,12 @@ class Person extends Component {
     this.inputElementRef = React.createRef();
   }
 
+  static contextType = AuthContext;
+
   componentDidMount() {
     // this.inputElement.focus();
     this.inputElementRef.current.focus();
+    console.log(this.context.authenticated);
   }
 
   render() {
@@ -22,11 +25,12 @@ class Person extends Component {
     return (
       // you can use React.Fragment from 'react' instead
       <Aux>
-        <AuthContext.Consumer>
-          {(context) =>
-            context.authenticated ? <p>Authenticated!</p> : <p>Please log in</p>
-          }
-        </AuthContext.Consumer>
+        {this.context.authenticated ? (
+          <p>Authenticated!</p>
+        ) : (
+          <p>Please log in</p>
+        )}
+
         <p onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old!
         </p>
