77befafbbdc3309925ac9e48c7f33675f8986942
Bartek <bartlomiejnawoj@gmail.com>
2020-10-08 19:24:50 +0200

    s7:106 another form of HOC (smth like decorators)

--- src/hoc/WithClass.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const withClass = (props) => (
-  // it sets up a class on a div that wraps a component
-  <div className={props.classes}>{props.children}</div>
-);
-
-export default withClass;
