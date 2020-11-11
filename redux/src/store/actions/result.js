c98eb7dd88faa64aca9ef96f1c09c208a2d3cf90
Bartek <bartlomiejnawoj@gmail.com>
2020-11-11 22:46:52 +0100

    using Utility Functions

--- redux/src/store/actions/result.js
+++ redux/src/store/actions/result.js
@@ -9,8 +9,10 @@ export const saveResult = (result) => {
 
 export const storeResult = (result) => {
 	// just a simplification of a request to server
-	return (dispatch) => {
+	return (dispatch, getState) => {
 		setTimeout(() => {
+			// const oldCounter = getState().ctr.counter;
+			// console.log(oldCounter);
 			dispatch(saveResult(result));
 		}, 2000);
 	};
