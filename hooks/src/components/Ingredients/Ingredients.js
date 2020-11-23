d8c3f51699c6026cff036c810585d6cc74b26cd3
Bartek <bartlomiejnawoj@gmail.com>
2020-11-23 18:44:12 +0100

    Using the custom hook

--- hooks/src/components/Ingredients/Ingredients.js
+++ hooks/src/components/Ingredients/Ingredients.js
@@ -28,11 +28,8 @@ const Ingredients = () => {
 		sendRequest,
 		reqExtra,
 		reqIdentifier,
+		clear,
 	} = useHttp();
-
-	// const [userIngredients, setUserIngredients] = useState([]);
-	// const [isLoading, setIsLoading] = useState(false);
-	// const [error, setError] = useState();
 
 	useEffect(() => {
 		if (!isLoading && !error && reqIdentifier === 'REMOVE_INGREDIENT') {
@@ -46,7 +43,6 @@ const Ingredients = () => {
 	}, [data, reqExtra, reqIdentifier, isLoading, error]);
 
 	const filteredIngredientsHandler = useCallback((filteredIngredients) => {
-		// setUserIngredients(filteredIngredients);
 		dispatch({ type: 'SET', ingredients: filteredIngredients });
 	}, []);
 
@@ -59,29 +55,6 @@ const Ingredients = () => {
 				ingredient,
 				'ADD_INGREDIENT'
 			);
-
-			// dispatchHttp({ type: 'SEND' });
-			// fetch(
-			// 	'https://react-hooks-course-2b20a.firebaseio.com/ingredients.json',
-			// 	{
-			// 		method: 'POST',
-			// 		body: JSON.stringify(ingredient),
-			// 		headers: { 'Content-Type': 'application/json' },
-			// 	}
-			// ).then((response) => {
-			// 	// dispatchHttp({ type: 'RESPONSE' });
-			// 	return response.json();
-			// });
-			// .then((responseData) => {
-			// 	// setUserIngredients((prevIngredients) => [
-			// 	// 	...prevIngredients,
-			// 	// 	{ id: responseData.name, ...ingredient },
-			// 	// ]);
-			// 	dispatch({
-			// 		type: 'ADD',
-			// 		ingredient: { id: responseData.name, ...ingredient },
-			// 	});
-			// });
 		},
 		[sendRequest]
 	);
@@ -99,10 +72,6 @@ const Ingredients = () => {
 		[sendRequest]
 	);
 
-	const clearError = useCallback(() => {
-		// dispatchHttp({ type: 'CLEAR' });
-	}, []);
-
 	const ingredientList = useMemo(() => {
 		return (
 			<IngredientList
@@ -114,7 +83,7 @@ const Ingredients = () => {
 
 	return (
 		<div className="App">
-			{error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+			{error && <ErrorModal onClose={clear}>{error}</ErrorModal>}
 			<IngredientForm
 				onAddIngredient={addIngredientHandler}
 				loading={isLoading}
